f40d6483b34a9001f1d340a77c535fc28b9a7868
phutran955 <trangiaphu54@gmail.com>
2026-01-08 14:55:27 +0700

    update timer và bg và mascot cho từng level

---
+++ js/configs/levelConfig.js
@@ -0,0 +1,19 @@
+export const levelConfig = {
+  1: {
+    background: "assets/images/level1bg.jpg",
+    mascot: "cat",
+    popupText: {
+      correct: "Giỏi lắm 😸",
+      wrong: "Sai rồi nè 😿",
+    },
+  },
+
+  2: {
+    background: "assets/backgrounds/level2.png",
+    mascot: "bear",
+    popupText: {
+      correct: "Tuyệt vời 🐻",
+      wrong: "Thử lại nhé 🐻‍❄️",
+    },
+  },
+};
