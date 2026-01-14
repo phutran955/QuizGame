ce6891e75be6cbd9d13d354519f318a478382958
HungLNQ <quochungle2005@gmail.com>
2026-01-14 14:16:40 +0700

    backup

--- js/components/SettingMenu.js
+++ js/components/SettingMenu.js
@@ -1,7 +1,7 @@
-import {
-  playSound,
-  toggleBackgroundMusic,
-  setMusicVolume,
+import { 
+  playSound, 
+  toggleBackgroundMusic, 
+  setMusicVolume, 
   getMusicVolume,
   isMusicPlaying
 } from "./soundManager.js";
@@ -12,12 +12,16 @@ export default function SettingMenu({ onClose, onGoStart, onGoLevel, onReplay })
 
   overlay.innerHTML = `
     <div class="setting-menu">
+
+<<<<<<< HEAD
+      <div class="setting-buttons">
+=======
       <div class="setting-group">
         <label>🎵 Âm lượng nhạc nền</label>
         <input id="music-range" type="range" min="0" max="1" step="0.01">
-        </div>
-      <div class="setting-buttons">
-        
+      </div>
+
+>>>>>>> Phu_feature
       <button id="btn-start">Về màn hình Start</button>
       <button id="btn-level">Chọn Level</button>
       <button id="btn-replay">Chơi lại Level</button>
@@ -51,20 +55,20 @@ export default function SettingMenu({ onClose, onGoStart, onGoLevel, onReplay })
 
   const btnMusic = overlay.querySelector("#btn-music");
 
-  function updateMusicButton() {
-    if (isMusicPlaying()) {
-      btnMusic.textContent = "🔇 Tắt nhạc";
-    } else {
-      btnMusic.textContent = "🎵 Bật nhạc";
-    }
+function updateMusicButton() {
+  if (isMusicPlaying()) {
+    btnMusic.textContent = "🔇 Tắt nhạc";
+  } else {
+    btnMusic.textContent = "🎵 Bật nhạc";
   }
+}
 
+updateMusicButton();
+
+btnMusic.onclick = () => {
+  toggleBackgroundMusic();
   updateMusicButton();
-
-  btnMusic.onclick = () => {
-    toggleBackgroundMusic();
-    updateMusicButton();
-  };
+};
 
 
   overlay.querySelector("#btn-close").onclick = () => {
